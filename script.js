341267650e215afdae18482e37319f532d8603f9
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-04-27 13:42:50 +0200

    The array is updated with the players selected

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const gameBoard = (function()
 	const gameGrid = [["", "", ""], ["", "", ""] , ["", "", ""]];
 	const updateGameGrid = (x_index, y_index, value) =>
 	{
-		gameGrid[y_index][x_index] = value;
+		if (!gameGrid[y_index][x_index])
+			gameGrid[y_index][x_index] = value;
 	}
 	return {updateGameGrid};
 })();
@@ -12,11 +13,18 @@ const gameBoard = (function()
 const gameMaster = (function()
 {
 	const gameGridDom = document.querySelector("#grid-game");
+	const playerTurnText = document.querySelector("#player-turn");
+	const gameResultText = document.querySelector("#game-result");
+
 	gameGridDom.addEventListener("click", (e)=>
 	{
 		const coords = e.target.dataset.coords.split('-');
 		if (e.target.className === "game-cell")
-			alert(`Target position is ${coords[0]}y and ${coords[1]}x`);
+		{
+			gameBoard.updateGameGrid(parseInt(coords[1]), parseInt(coords[0]), players.getCurrentPlayerMarker());
+			players.changePlayerTurn();
+		}
+			//alert(`Target position is ${coords[0]}y and ${coords[1]}x`);
 	});
 	return {gameGridDom};
 })();
@@ -24,8 +32,15 @@ const gameMaster = (function()
 
 const players = (function()
 {
-	let player1Name;
-	let player2Name;
+	const player1 = createPlayer(null, 'X');
+	const player2 = createPlayer(null, 'O');
+
+	let currentPlayerTurn = "Player 1";
+
+	function createPlayer(name, marker)
+	{
+		return {name, marker};
+	};
 	const setPlayerNames = () =>
 	{
 		let player1Input;
@@ -34,13 +49,24 @@ const players = (function()
 			player1Input = prompt("Enter player 1 name");
 		while (!player2Input)
 			player2Input = prompt("Enter player 2 name");
-		player1Name = player1Input;
-		player2Name = player2Input;
-	}
+		player1.name = player1Input;
+		player2.name = player2Input;
+	};
 	const getPlayerNames = () =>
 	{
-		return [player1Name, player2Name];
+		let player1Name = player1.name;
+		let player2Name = player2.name;
+		return {player1Name, player2Name};
+	};
+	const changePlayerTurn = () =>
+	{
+		currentPlayerTurn = currentPlayerTurn === "Player 1" ? "Player 2" : "Player 1";
+		return (currentPlayerTurn); 
+	};
+	const getCurrentPlayerMarker = () =>
+	{
+		return (currentPlayerTurn === "Player 1" ? "X" : "O");
 	}
-	return {setPlayerNames, getPlayerNames};
+	return {setPlayerNames, getPlayerNames, changePlayerTurn, getCurrentPlayerMarker};
 })();
 
