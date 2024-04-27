7de3d87bdfca0262021907522b434af1beda9306
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-04-27 12:24:55 +0200

    We store the player names correctly and there is an event listener on click

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const gameBoard = (function()
 {
 	const gameGrid = [["", "", ""], ["", "", ""] , ["", "", ""]];
-	const updateGameGrid = function(x_index, y_index, value)
+	const updateGameGrid = (x_index, y_index, value) =>
 	{
 		gameGrid[y_index][x_index] = value;
 	}
@@ -9,4 +9,38 @@ const gameBoard = (function()
 })();
 
 
+const gameMaster = (function()
+{
+	const gameGridDom = document.querySelector("#grid-game");
+	gameGridDom.addEventListener("click", (e)=>
+	{
+		if (e.target.className === "game-cell")
+			alert("Cells are working");
+	});
+	return {gameGridDom};
+})();
 
+
+const players = (function()
+{
+	let player1Name;
+	let player2Name;
+	const definePlayerNames = (player1Input, player2Input) =>
+	{
+		player1Name = player1Input;
+		player2Name = player2Input;
+	}
+	const getPlayerNames = () =>
+	{
+		let player1Input;
+		let player2Input;
+		while (!player1Input)
+			player1Input = prompt("Enter player 1 name");
+		while (!player2Input)
+			player2Input = prompt("Enter player 2 name");
+		definePlayerNames(player1Input, player2Input);
+	}
+	return {getPlayerNames};
+})();
+
+players.getPlayerNames();
